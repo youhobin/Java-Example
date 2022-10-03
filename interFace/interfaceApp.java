49b71bca1398cfd3089e2969ae7b9d5174530920
youhobin <ghdb132@naver.com>
2022-10-03 12:36:37 +0900

    Add files via upload

---
+++ interFace/interfaceApp.java
@@ -0,0 +1,17 @@
+interface Calculable{
+	int sum(int v1, int v2);
+}
+class RealCal implements Calculable{
+	public int sum(int v1, int v2) {
+		return v1 + v2;
+	}
+}
+public class interfaceApp {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
